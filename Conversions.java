3b49d8b097b1f423df11d187e99cb4e2afe9eacf
SabraHummusMan <155586870+JamesJunwonSong@users.noreply.github.com>
2025-08-20 18:56:29 +0000

    Made Menu with Options 1-6

--- Conversions.java
+++ Conversions.java
@@ -1,1 +1,15 @@
+// James Song
+// 08/20/2025
 
+public class Conversions {
+
+    public static void main(String[] args) {
+        // Menu Optons
+        System.out.println("1. Farenheit to Celsius");
+        System.out.println("2. Celsius to Farenheit");
+        System.out.println("3. Feet to Meters");
+        System.out.println("4. Meters to Feet");
+        System.out.println("5. Ounces to Milliliters");
+        System.out.println("6. Milliliters to Ounces");
+    }
+}
